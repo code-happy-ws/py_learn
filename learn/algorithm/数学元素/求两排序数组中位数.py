71b907447b09b7d3d04d4ec7e039a5d35ab68968
code-happy-ws <1090849088@qq.com>
2020-06-23 08:45:21 +0800

    update

--- learn/algorithm/数学元素/求两排序数组中位数.py
+++ learn/algorithm/数学元素/求两排序数组中位数.py
@@ -1,6 +1,38 @@
 """给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
 """
+def get_mid_num_by_bisect(m,n):
+    """二分法：转化问题为求第k大数，k为中位数位置"""
+    def get_k(k):
+        index1,index2=0,0
+        while True:
+            # 特殊情况
+            if index1 == len_m:
+                return n[index2+k-1]
+            if index2==n:
+                return m[index1+k-1]
+            if k==1:
+                return min(m[index1],n[index2])
+
+            # 正常情况
+            new_index1 = min(index1+k//2-1,len_m-1)
+            new_index2 = min(index2+k//2-1,len_n-1)
+            elem1,elem2 = m[new_index1],n[new_index2]
+            if elem1<=elem2:
+                k-=new_index1-index1+1
+                index1 = new_index1+1
+            else:
+                k-=new_index2-index2+1
+                index2=new_index2+1
+
+    len_m,len_n=len(m),len(n)
+    len_m_n=len_m+len_n
+    if len_m_n%2:
+        return get_k((len_m_n+1)//2)
+    else:
+        return (get_k(len_m_n//2)+get_k(len_m_n//2+1))/2
+
+
 
 def get_mid_num(m,n):
     # 普通方法 O(m+n) 遍历len(m+n)/2+1次
@@ -32,3 +64,4 @@ if __name__ == '__main__':
     m = [1,5]
     n = [4]
     print(get_mid_num(m,n))
+    print(get_mid_num_by_bisect(m,n))
